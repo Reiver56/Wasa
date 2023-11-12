95d982a23957d9db57a2a3c02c9774baf2a4b1a9
matteocarrese <carrese.1905917@studenti.uniroma1.it>
2023-11-12 22:48:02 +0100

    1

---
+++ service/api/api-handler.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"net/http"
+)
+
+// Handler returns an instance of httprouter.Router that handle APIs registered here
+func (rt *_router) Handler() http.Handler {
+	// Register routes
+
+}
