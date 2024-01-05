c979591c8feeddfa74409276bdc867004f78f60f
Reiver56 <carrese.1905917@studenti.uniroma1.it>
2024-01-05 03:53:04 +0100

    [fixed some frontend's elements and modified Docker files ]

---
+++ .history/webui/src/components/LikeModal_20240105021107.vue
@@ -0,0 +1,33 @@
+<script>
+export default{
+	props: ['modal_id', 'likes'],
+}
+</script>
+
+<template>
+	<div class="modal fademodal-dialog modal-dialog-centered modal-dialog-scrollable my-modal-disp-none" :id="modal_id" tabindex="-1" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title fs-5">Likes</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class= "modal-body">
+					<UserMini v-for="user in likes"
+					:identifier="user.id_user"
+					/>
+
+				</div>
+
+		</div>
+
+	</div>
+	</div>
+
+</template>
+
+<style>
+.my-modal-disp-none{
+	display: none;
+}
+</style>
---
+++ .history/webui/src/components/NavBar_20240105011752.vue
@@ -0,0 +1,118 @@
+<template>
+	<ErrorMsg v-if="errorMsg" :msg="errorMsg" @close-error="errorMsg =''"></ErrorMsg>
+	<div class="navbar">
+		<img src="../assets/logo/gallery.png" alt="logo" width="50" height="50">
+		<SearchBar @error-occurred="$emit('error-occurred', value)" />
+		<button class="btn home-button" @click="$router.go(0)">
+			<font-awesome-icon icon="fa-solid fa-house" />
+		</button>
+		<button @click="getMyProfile" class="btleft-profile-navbar">
+			<!--Profile-->
+			<font-awesome-icon icon="fa-solid fa-user" />
+		</button>
+
+
+	</div>
+</template>
+
+<script>
+import SearchBar from './SearchBar.vue';
+export default{
+	components:{
+		SearchBar,
+	},
+	emits: ['error-occurred'],
+	data(){
+		return{
+			errorMsg:'',
+		}
+
+	},
+	methods:{
+		async getMyProfile(){
+
+			this.$router.push(`/users/${localStorage.UserID}`);
+
+
+		},
+	},
+
+	mounted(){
+
+	},
+}
+</script>
+
+<style>
+.navbar{
+	background-color: #3b9ee4;
+    height: auto;
+    width: 100%;
+    padding: 0;
+    box-shadow: 0 0.2em 0 0 rgba(176, 179, 184, 0.50);
+    position: sticky;
+    padding: 1em;
+    margin-bottom: 1em;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    z-index: 3;
+}
+.left-profile-navbar {
+    background-color: rgb(255, 255, 255, 0.4);
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 10em;
+    padding: 0;
+    margin: 0 0 0 0.5em;
+    float: left;
+    outline: none;
+    border: none;
+    font-size: 2em;
+    line-height: 0;
+    color: rgb(0, 0, 0, 0.8)
+
+}
+.left-profile-navbar:hover{
+	cursor: pointer;
+	background-color: rgb(255, 255, 255, 0.8);
+}
+.home-button{
+	background-color: rgb(255, 255, 255, 0.4);
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 10em;
+    padding: 0;
+    margin: 0 0 0 0.5em;
+    float: left;
+    outline: none;
+    border: none;
+    font-size: 2em;
+    line-height: 0;
+    color: rgb(0, 0, 0, 0.8)
+}
+
+.home-button:hover{
+	cursor: pointer;
+	background-color: rgb(255, 255, 255, 0.8);
+}
+.upload-post-button{
+	background-color: rgb(255, 255, 255, 0.4);
+	height: 1.5em;
+	width: 1.5em;
+	border-radius: 10em;
+	padding: 0;
+	margin: 0 0 0 0.5em;
+	float: left;
+	outline: none;
+	border: none;
+	font-size: 2em;
+	line-height: 0;
+	color: rgb(0, 0, 0, 0.8)
+}
+
+.upload-post-button:hover{
+	cursor: pointer;
+	background-color: rgb(255, 255, 255, 0.8);
+}
+</style>
